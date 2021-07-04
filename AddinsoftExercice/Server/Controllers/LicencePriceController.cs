b0905215e25a5417622239e3481dc7c327e87b07
Orel <aurelien.dellac@viacesi.fr>
2021-07-04 16:39:42 -0400

    Api setup

---
+++ AddinsoftExercice/Server/Controllers/LicencePriceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LicencePriceController : ControllerBase
+    {
+        // GET: api/<LicencePriceController>
+        [HttpGet]
+        public ActionResult<LicencePrice> Get(int quantity, string currency)
+        {
+            if (quantity <= 0)
+            {
+                return BadRequest("Quantity must be more than 0");
+            }
+
+            //handle bad currency
+            LicencePrice quote = new LicencePrice();
+            quote.Quantity = quantity;
+            quote.Currency = currency;
+            return quote;
+        }
+    }
+}
---
+++ AddinsoftExercice/Server/Models/LicencePrice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class LicencePrice
+    {
+        public double Unit { get; set; }
+        public double Total { get; set; }
+        public int Quantity { get; set; }
+        public String Currency { get; set; }
+    }
+}
