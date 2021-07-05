607d3aa7060e883ffe0458c96a944812701a406d
Orel <aurelien.dellac@viacesi.fr>
2021-07-05 01:06:16 -0400

    remove useless code

--- AddinsoftExercice/Client/MainWindow.xaml.cs
+++ AddinsoftExercice/Client/MainWindow.xaml.cs
@@ -24,10 +24,5 @@ namespace Client
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
