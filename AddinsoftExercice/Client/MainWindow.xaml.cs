61c1df2d31c9ba9d35ef5769fa9eed71c657c45b
Orel <aurelien.dellac@viacesi.fr>
2021-07-05 01:05:19 -0400

    ui client v1

--- AddinsoftExercice/Client/MainWindow.xaml.cs
+++ AddinsoftExercice/Client/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
