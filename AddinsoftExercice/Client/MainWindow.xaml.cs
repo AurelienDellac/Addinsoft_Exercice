0bf222619c3fe9b77707a684209c4f5edec3cd1b
Orel <aurelien.dellac@viacesi.fr>
2021-07-05 04:17:32 -0400

    des corrections

--- AddinsoftExercice/Client/MainWindow.xaml.cs
+++ AddinsoftExercice/Client/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Client.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,18 +33,28 @@ namespace Client
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            licencePricePreview.Visibility = Visibility.Hidden;
+            loading.Visibility = Visibility.Visible;
             int quantity;
 
             if(int.TryParse(licenceNumber.Text, out quantity))
             {
-                licencePricePreview.Text = await licenceRepository.GetLicencePricePreview(
-                    quantity,
-                    currency.Text);
+                try
+                {
+                    licencePricePreview.Text = await licenceRepository.GetLicencePricePreview(
+                        quantity,
+                        currency.Text);
+                } catch (HttpRequestException httpException)
+                {
+                    licencePricePreview.Text = httpException.Message;
+                }
+                
             } else
             {
                 licencePricePreview.Text = "La quantité de licence indiquée n'est pas correcte !";
             }
-         
+
+            loading.Visibility = Visibility.Hidden;
             licencePricePreview.Visibility = Visibility.Visible;
         }
     }
